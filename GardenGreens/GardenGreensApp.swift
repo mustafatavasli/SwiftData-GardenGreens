e3e2ea32d551f28f480a140a1425da2c4a38680d
mustafatavasli <musstafatavasli@gmail.com>
2024-07-17 10:55:09 +0300

    Initial Commit

---
+++ GardenGreens/GardenGreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenGreensApp.swift
+//  GardenGreens
+//
+//  Created by Mustafa TAVASLI on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GardenGreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
